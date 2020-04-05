1d40516a1ea52c0aa2e1e3a1e4439698ba24ed82
gyusika <gyusika@naver.com>
2020-04-06 03:28:42 +0900

    수정

--- src/components/profile-card.js
+++ src/components/profile-card.js
@@ -1,66 +1,110 @@
 import React from "react";
 import styled from "styled-components";
 
-export default function ProfileCard() {
-  const profiles = [
-    {
-      id: 1,
-      name: "박효진",
-      age: 23,
-      role: "개발 파트장",
-      univ: "이화여자",
-      major: "컴퓨터공학",
-      phoneNum: "010-5106-6785",
-      email: "gywls517@gmail.com",
-      githubLink: "https://github.com/gywlsp",
-      imageUrl:
-        "https://static.wixstatic.com/media/69e8c2_982cbc3d638e49889d8ba64b3c70a6e3~mv2.jpeg/v1/crop/x_199,y_0,w_543,h_560/fill/w_350,h_347,al_c,q_80,usm_0.66_1.00_0.01/%E1%84%8E%E1%85%AC%E1%84%92%E1%85%AD%E1%84%8C%E1%85%B5%E1%86%AB.webp"
-    },
-    {
-      id: 2,
-      name: "양병훈",
-      age: 25,
-      univ: "서강",
-      role: "운영 팀원",
-      major: "컴퓨터공학",
-      phoneNum: "010-8200-1760",
-      email: "ybh1760@gmail.com",
-      githubLink: "https://github.com/ybh1760",
-      imageUrl:
-        "https://static.wixstatic.com/media/69e8c2_3de533d9d5ee4bbaa5fe0c360e8f6a23~mv2.jpeg/v1/crop/x_0,y_456,w_3024,h_3119/fill/w_351,h_347,al_c,q_80,usm_0.66_1.00_0.01/%E1%84%8B%E1%85%A3%E1%86%BC%E1%84%87%E1%85%A7%E1%86%BC%E1%84%92%E1%85%AE%E1%86%AB.webp"
-    },
-    {
-      id: 3,
-      name: "이수완",
-      age: 22,
-      univ: "이화여자",
-      role: "홍보 파트장",
-      major: "컴퓨터공학",
-      phoneNum: "010-5503-7994",
-      email: "2swan965@gmail.com",
-      githubLink: "https://github.com/lee-soowan65",
-      imageUrl:
-        "https://static.wixstatic.com/media/982853_a188a89325cb4a20b6a487f7e3de5d14~mv2.jpg/v1/crop/x_13,y_1126,w_2998,h_2829/fill/w_351,h_335,al_c,q_80,usm_0.66_1.00_0.01/4A2633DE-3322-4CDC-8F8C-35288D83C43B-500.webp"
-    },
-    {
-      id: 4,
-      name: "최수민",
-      age: 26,
-      univ: "서강",
-      role: "부회장",
-      major: "컴퓨터공학",
-      phoneNum: "010-4413-1261",
-      email: "react@kakao.com",
-      githubLink: "https://github.com/greatSumini",
-      imageUrl:
-        "https://static.wixstatic.com/media/69e8c2_43446e9ac65e4f129083516175259a2b~mv2.jpeg/v1/crop/x_0,y_910,w_3024,h_3122/fill/w_347,h_351,al_c,q_80,usm_0.66_1.00_0.01/%E1%84%8E%E1%85%AC%E1%84%89%E1%85%AE%E1%84%86%E1%85%B5%E1%86%AB.webp"
-    }
-  ];
-
-  return <Wrapper>안녕 나는 프로필 카드</Wrapper>;
+export default function ProfileCard({
+   id,
+   name,
+   age,
+   role,
+   univ,
+   major,
+   phoneNum,
+   email,
+   githubLink,
+   imageUrl
+}) {
+  return (
+    <Wrapper>
+      <InfoWrapper>
+        <Info>
+          <Up>
+            <Name>{name}({age})</Name>
+            <Role>{role}</Role>
+            <Univ>{univ}대학교 {major}과</Univ>
+          </Up>
+          <Bottom>
+            <PhoneNum>{phoneNum}</PhoneNum>
+            <Mail>{email}</Mail>
+            <Git href={githubLink}>{githubLink}</Git>
+          </Bottom>
+        </Info>
+        <Img src={imageUrl}/>
+      </InfoWrapper>
+      <ClubName>신촌 연합 IT 창업 동아리 CEOS</ClubName>
+    </Wrapper>
+  )
 }
 
+const ClubName = styled.div`
+  width: 100%;
+  text-align: center;
+  font-size: 1.6rem;
+  margin: 0rem;
+`;
+
+const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
+  margin-bottom: 1.5rem;
+`;
+
+const Img = styled.img`
+  width: 20rem;
+  height: 20rem;
+`;
+
+const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
+  width: 50%;
+`;
+
+const Up = styled.div`
+  box-sizing: border-box; 
+`;
+
+const Bottom = styled.div`
+  box-sizing: border-box; 
+`;
+
+const Name = styled.div`
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 1rem;
+`; 
+
+const Role = styled.div`
+  font-size: 1.4rem;
+  font-weight: bold;
+`;
+
+const Univ = styled.div`
+  font-size: 1.4rem;
+`;
+
+const PhoneNum = styled.div`
+  font-size: 1.4rem;
+  color: rgb(0, 0, 153);
+`;
+
+const Mail = styled.div`
+  font-size: 1.4rem;
+  color: rgb(0, 0, 153);
+`;
+
+const Git = styled.a`
+  font-size: 1.4rem;
+`;
+
 const Wrapper = styled.div`
-  background-color: red;
-  font-size: 18px;
+  width: 45%;
+  background-color: rgb(255, 255, 255);
+  margin-bottom: 2rem;
+  padding: 1.5rem;
+  border-width: 1px solid rgb(204, 204, 204);
+  border-radius: 1.5rem;
 `;
